688cb9fc28205e639ad0e08c985c14f2162ca956
lazyoracle <contact@anuragsaharoy.me>
2021-12-11 13:05:58 +0100

    fix y->x typo

--- Project1/Matlab Files/gen_sim_data.m
+++ Project1/Matlab Files/gen_sim_data.m
@@ -3,7 +3,7 @@ function x = gen_sim_data(m, k, d, x0, dt, N)
     tf = t0 + (dt * N);
     tspan = [t0 tf];
     opts = odeset('InitialStep', dt, 'MaxStep', dt);
-    sol = ode45(@(t,x) spring_diff(t,y,m,k,d), tspan, x0, opts);
+    sol = ode45(@(t,x) spring_diff(t,x,m,k,d), tspan, x0, opts);
     t = linspace(t0, tf, N);
     x = deval(sol, t);
 end
