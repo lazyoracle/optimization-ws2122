5bac9d4cbaf39e6db562befd7a4eed9d5d7c7a63
lazyoracle <contact@anuragsaharoy.me>
2021-12-11 19:23:44 +0100

    create and pass A and b matrix to fminunc

--- Project1/Matlab Files/get_optim_signal.m
+++ Project1/Matlab Files/get_optim_signal.m
@@ -1,8 +1,28 @@
-function optim_signal = get_optim_signal(y_raw)
-    y_den_0 = zeros(size(y_raw));
-    options = optimoptions("fmincon", "Display", "iter-detailed", ...
-        "OptimalityTolerance", 1e-10);
+function optim_signal = get_optim_signal(y_raw, t)
+    meas_size = size(y_raw);
+    y_den_0 = zeros(meas_size);
+    options = optimoptions("fmincon", "Display", "final-detailed", ...
+        "OptimalityTolerance", 1e-10, ...
+        "MaxFunctionEvaluations", 1e5);
+    
+    % A & B matrix for linear inequality constraint
+    % We need (f(y2) - f(y1)) / (t2 - t1) <= 1
+    % or f(y2) - f(y1) <= t2 - t1
+    % A matrix can thus be the forward difference operator
+    % b matrix can be diff(t) with some shape adjustments
+    b = diff(t);
+    b(end + 1) = t(end);
+    A = - eye(meas_size(2));
+    for m = 1:meas_size(2)
+        for n = 1:meas_size(2)
+            if n == m+1
+                A(m, n) = 1;
+            end
+        end
+    end
+
+
     optim_signal = fmincon(@(y_den)get_signal_distance(y_den,y_raw), ...
-        y_den_0, [], [], [], [], [], [], [], ...
+        y_den_0, A, b, [], [], [], [], [], ...
         options);
 end
