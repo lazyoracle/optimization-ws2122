52eb3bca4e72c67d59ad582872b6052f4997062f
lazyoracle <contact@anuragsaharoy.me>
2021-12-11 13:47:17 +0100

    works but doesnt give scalars

---
+++ Project1/Matlab Files/get_residuum.m
@@ -0,0 +1,13 @@
+function res = get_residuum(param,sim_param)
+    m = param(1);
+    k = param(2);
+    d = param(3);
+    x0 = cell2mat(sim_param(1));
+    dt = cell2mat(sim_param(2));
+    N = cell2mat(sim_param(3));
+    x_star_noise = cell2mat(sim_param(4)).x_star_noise;
+    x_sim = gen_sim_data(m, k, d, x0, dt, N);
+    errs = x_star_noise - x_sim';
+    errr_sq = errs.^2;
+    res = sum(errr_sq);
+end
