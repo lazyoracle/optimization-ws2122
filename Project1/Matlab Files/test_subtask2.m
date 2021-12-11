10024204d912a799660481b25cc42a43dc59c6e7
lazyoracle <contact@anuragsaharoy.me>
2021-12-11 16:14:53 +0100

    part e, comparison with lpf

--- Project1/Matlab Files/test_subtask2.m
+++ Project1/Matlab Files/test_subtask2.m
@@ -1,20 +1,42 @@
 clearvars;
 clear;
 load("measurements.mat");
+Y_LABEL = "Signal Values";
+Y_LABEL_DIFF = "Signal Derivative";
+X_LABEL = "Time in s";
 
+% Plot noisy signal
 subplot(2, 3, 1);
 plot(t, y_raw)
 hold on
 plot(t, y)
 title("Noisy Measurement");
-ylabel("Signal Values");
+ylabel(Y_LABEL);
 
+% Plot noisy signal derivative
 y_raw_df = diff(y_raw)./diff(t);
 y_df = diff(y)./diff(t);
-
 subplot(2, 3, 4);
 plot(t(1:end-1), y_raw_df)
 hold on
 plot(t(1:end-1), y_df)
-ylabel("Signal Derivative");
+ylabel(Y_LABEL_DIFF);
+xlabel("Time in s");
+
+% Plot lpf signal
+y_lpf = easy_low_pass(y_raw, 0.6);
+subplot(2, 3, 3);
+plot(t, y_lpf)
+hold on
+plot(t, y)
+title("Low-pass Filter");
+ylabel(Y_LABEL);
+
+% Plot lpf signal derivative
+y_lpf_df = diff(y_lpf)./diff(t);
+subplot(2, 3, 6);
+plot(t(1:end-1), y_lpf_df)
+hold on
+plot(t(1:end-1), y_df)
+ylabel(Y_LABEL_DIFF);
 xlabel("Time in s");
