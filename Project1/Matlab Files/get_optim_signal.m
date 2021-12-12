b063985aa5ed6928a28e55436756870328a8b2a7
lazyoracle <contact@anuragsaharoy.me>
2021-12-12 19:19:54 +0100

    add comments explaining A and B matrix

--- Project1/Matlab Files/get_optim_signal.m
+++ Project1/Matlab Files/get_optim_signal.m
@@ -8,11 +8,14 @@ function optim_signal = get_optim_signal(y_raw, t)
     % A & B matrix for linear inequality constraint
     % We need (f(y2) - f(y1)) / (t2 - t1) <= 1
     % or f(y2) - f(y1) <= t2 - t1
-    % A matrix can thus be the forward difference operator
-    % b matrix can be diff(t) with some shape adjustments
+    % We also need (f(y1) - f(y2)) / (t2 - t1) <= 1
+    % A matrix can thus be the forward difference and
+    % backward difference operator augmented together
+    % b matrix can be diff(t) augmented with itself
     b = diff(t);
-    b(end + 1) = t(end);
-    b = [b b];
+    b(end + 1) = t(end); % diff(t) is one elem smaller
+    b = [b b]; % augment b with itself
+    
     A = zeros(2 * meas_size(2), meas_size(2));
     m_max = size(A, 1);
     n_max = size(A, 2);
