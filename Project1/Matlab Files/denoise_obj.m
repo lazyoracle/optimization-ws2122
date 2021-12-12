ea0942fbb7300119278f9824c25491f117bbff14
lazyoracle <contact@anuragsaharoy.me>
2021-12-12 20:01:32 +0100

    control weighing of J1 J2 with function arg smooth

--- Project1/Matlab Files/denoise_obj.m
+++ Project1/Matlab Files/denoise_obj.m
@@ -1,5 +1,7 @@
-function obj = denoise_obj(y_den, y_raw)
+function obj = denoise_obj(y_den, y_raw, smooth)
+    % smooth controls the weighing of J1 and J2
+    % smooth should be between (0, 1) 
     J1 = get_signal_distance(y_den, y_raw);
     J2 = get_regularization(y_den);
-    obj = (0.3 * J1) + (0.7 * J2);
+    obj = ((1 - smooth) * J1) + (smooth * J2);
 end
