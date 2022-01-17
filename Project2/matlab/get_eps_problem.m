9acde787a58c40b130f48ab65e90737e25d52042
lazyoracle <contact@anuragsaharoy.me>
2022-01-17 21:44:15 +0100

    basic eps constraint problem struct generator

---
+++ Project2/matlab/get_eps_problem.m
@@ -0,0 +1,17 @@
+function eps_problem = get_eps_problem(cost_fun, eps, x0, lb, ub, nonlcon)
+    cost_fun1 = cost_fun(1);
+    cost_fun2 = cost_fun(2);
+
+    function [c,ceq] = constraint(x)
+        c(1) = cost_fun2(x) - eps;
+        c(2) = nonlcon(x);
+        ceq = [];
+    end
+
+    eps_problem.objective = cost_fun1;
+    eps_problem.x0 = x0;
+    eps_problem.solver = 'fmincon';
+    eps_problem.lb = lb;
+    eps_problem.ub = ub;
+    eps_problem.nonlcon = constraint;    
+end
