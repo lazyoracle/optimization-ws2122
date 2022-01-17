09c4cff196f046941c3206518e4318061bc38d59
lazyoracle <contact@anuragsaharoy.me>
2022-01-17 20:50:20 +0100

    basic ws problem struct generator

---
+++ Project2/matlab/get_ws_problem.m
@@ -0,0 +1,13 @@
+function ws_problem = get_ws_problem(cost_fun, weights, x0, lb, ub)
+    cost_fun1 = cost_fun(1);
+    cost_fun2 = cost_fun(2);
+    weighted_sum = @(x) weights(1) * cost_fun1(x) + weights(2) * cost_fun2(x);
+    ws_problem.objective = weighted_sum;
+    ws_problem.x0 = x0;
+    ws_problem.solver = 'fmincon';
+    ws_problem.lb = lb;
+    ws_problem.ub = ub;
+    
+
+
+    
